0ce60d8e0a38c30049f9cc6a97716f39b6a74d16
Itz <42345781+itzelalejandra213@users.noreply.github.com>
2018-08-20 08:47:05 -0500

    subí archivos manualmente
    prueba de github

---
+++ holam/src/escul/Alumno.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escul;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Alumno {
+    private String nombre;
+    private String apellidos;
+    private String edad;
+    private String boleta; 
+}
